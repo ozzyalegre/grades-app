42362485d3c6f3cf3603ca3433e67ab931b9b995
ozzyalegre <125777377+ozzyalegre@users.noreply.github.com>
2023-11-30 16:20:08 -0500

    minor front end changes

--- resources/views/livewire/grades-table.blade.php
+++ resources/views/livewire/grades-table.blade.php
@@ -14,6 +14,8 @@
               <tr>
                 <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Class</th>
                 @foreach ($terms as $t)
+                    
+                
                     @if ($t->id > $current_term->id || $t->id < $current_term->id)
                         <th scope="col" class="hidden md:table-cell px-3 py-3.5 text-left text-sm font-semibold text-gray-900">{{ $t->name }}</th>
                     @else
@@ -21,37 +23,32 @@
                     @endif
                     
                 @endforeach                
-                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Past 2 Weeks</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Lo/Avg/Hi</th>
               </tr>
             </thead>
             <tbody class="divide-y divide-gray-200">
-                
-                    @foreach ($subjects_grades_latest as $s)
+                @foreach ($subjects_grades_latest as $s)
                     <tr>
                         <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">{{ $s->subject_name }}</td>
-                        @foreach ($s->latest_grades as $g)
-                            @if ($g != null)
-                                @if ($g->term_id > $current_term->id || $g->term_id < $current_term->id)
-                                    <td class="hidden md:table-cell whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $g->gpa }} / {{ $g->letter }} </td>
-                                @else
+                            @foreach ($s->latest_grades as $g)
+                                @if ($g != null)
+                                    @if ($g->term_id > $current_term->id || $g->term_id < $current_term->id)
+                                        <td class="hidden md:table-cell whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $g->gpa }} / {{ $g->letter }} </td>
+                                    @else
                                     @if (floatval($g->gpa) <= 2.60)    
                                         <td class="whitespace-nowrap px-3 py-4 text-sm bg-gray-100 text-orange-500 font-bold">{{ $g->gpa }} / {{ $g->letter }} </td>
                                     @else
                                         <td class="whitespace-nowrap px-3 py-4 text-sm bg-gray-100 text-gray-500">{{ $g->gpa }} / {{ $g->letter }} </td>
                                     @endif
-
-                                @endif 
-                                
-                            @endif
-                            
-                        @endforeach
+                                    @endif 
+                                @endif
+                            @endforeach
                     </tr>
-                    @endforeach
-                
+                @endforeach
             </tbody>
           </table>
           <div class="mt-4 sm:mt-0 mr-4 sm:flex-none float-right">
-            <p class="mt-2 italic text-xs text-gray-700">Last Report: {{ $report_latest->date_received}}</p>
+            <p class="italic text-xs text-gray-700">Last Report: {{ $report_latest->date_received}}</p>
           </div>
         </div>
       </div>
