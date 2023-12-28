7dae2d226abfa82790eae04387d72e3a2a8b9c0b
ozzyalegre <125777377+ozzyalegre@users.noreply.github.com>
2023-12-28 10:53:39 -0500

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,9 +4,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-md rounded-md py-8">
                 {{-- Grades Table --}}
+                @dd($terms, $subjects);
                 <x-grades-table :$terms :$subjects :$currentTerm :$subjectsGrades />
-
-                
             </div>
             {{-- Subject History Charts --}}
             <div class="grid sm:grid-cols-4 gap-3">
