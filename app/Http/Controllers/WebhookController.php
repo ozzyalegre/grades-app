a4d7c395beb027fa38f403b4239ed0e54224bf86
ozzyalegre <125777377+ozzyalegre@users.noreply.github.com>
2023-12-28 13:02:55 -0500

    Update WebhookController.php

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,7 +9,7 @@ use App\Services\DataStorageService;
 class WebhookController extends Controller
 {
     public function processIncomingWebhook(Request $request){
-        $body = strval($request->all());
+        $body = $request->all();
         // $new_report = $this->parseAndStore($body);
         return response()->json(['message' => 'Report added successfully.', 'report' => $body, ], 201);
     }
