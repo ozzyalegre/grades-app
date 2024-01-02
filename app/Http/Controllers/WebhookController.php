5533e6ced714798707f402d7e13880bc28dc2b21
ozzyalegre <125777377+ozzyalegre@users.noreply.github.com>
2024-01-02 15:46:45 -0500

    webhook controller now working

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,20 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\ParseService;
 use App\Services\DataStorageService;
+use Carbon\Carbon;
+use stdClass;
 
 class WebhookController extends Controller
 {
     public function processIncomingWebhook(Request $request){
-        $body = $request->getContent();
-        logger($body);
-        $new_report = $this->parseAndStore($body);
-        return response()->json(['message' => 'Report added successfully.', 'request' => $new_report], 201);
+        $request = json_decode($request->getContent());
+        $message = new stdClass;
+        $message->mid = $request->mid;
+        $message->date_received = new Carbon($request->date_received, 'America/New_York');
+        $message->from = $request->from;
+        $message->body = $request->body;
+        try {
+            $new_report = $this->parseAndStore($message);
+            return response()->json(['message' => 'Report added successfully.', 'request' => $new_report->message_id], 201);
+        } catch (\Throwable $th) {
+            logger($th);
+            return response()->json(['message' => 'Report failed to be added.', 'error' => $th], 201);
+        }
     }
 
     public function parseAndStore($message){
         // Parse New Message
         $parser = new ParseService;
-        $parsed_class_grades = $parser->ParseHtml($message);
+        $parsed_class_grades = $parser->ParseHtml($message->body);
         $parsed_terms = $parser->GetTerm();
 
         // DataStorage Time!
